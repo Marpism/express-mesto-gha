4faa111a1d45437cd5906ef8e286d4ce5fd34e67
Mary O <marymary-o@ya.ru>
2023-08-04 17:00:28 +0300

    14th sprint/1st edition

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken');
+const UnauthError = require('../errors/UnauthError');
 const { UNAUTHORIZED } = require('../error_codes/errorCodes')
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(UNAUTHORIZED)
-      .send({ message: 'Необходима авторизация' });
+    // return res
+    //   .status(UNAUTHORIZED)
+    //   .send({ message: 'Необходима авторизация' });
+    throw new UnauthError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res.status(UNAUTHORIZED).send({ message: 'Необходима авторизация' });
+    next(err)
+    // return res.status(UNAUTHORIZED).send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
 
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,11 +3,22 @@ const { INTERNAL_SERVER_ERROR } = require('../error_codes/errorCodes')
 module.exports = ((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
-
+console.log(statusCode)
   res.status(statusCode).send({
       // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message
     });
+    console.log(statusCode)
+  next()
 });
+
+// module.exports = (err, req, res, next) => {
+//   const statusCode = err.statusCode ?? INTERNAL_SERVER_ERROR;
+//   const message = (statusCode === INTERNAL_SERVER_ERROR)
+//     ? 'Произошла ошибка сервера'
+//     : err.message;
+//   res.status(statusCode).send({ message });
+//   next();
+// };
