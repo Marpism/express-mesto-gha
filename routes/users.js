a5c89399e296445befb2b636539618d260755601
Mary O <marymary-o@ya.ru>
2023-08-06 21:27:08 +0300

    14th sprint/1st edition

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,17 @@ userRouter.get('/users/me', getCurrentUser);
 userRouter.get('/users/:_id', getUser);
 userRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
-    about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().pattern(regEx),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+    // avatar: Joi.string().pattern(regEx),
     // email: Joi.string().email().required(),
     // password: Joi.string().min(8).required(),
   }),
 }), updateUser);
-userRouter.patch('/users/me/avatar', updateAvatar);
+userRouter.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(regEx),
+  }),
+}), updateAvatar);
 
 module.exports = userRouter;
