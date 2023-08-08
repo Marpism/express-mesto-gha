f361f566d27fe89c957a72535234e308b29c1454
Mary O <marymary-o@ya.ru>
2023-08-08 12:50:00 +0300

    14th sprint/2nd edition/hotfixes2

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ userRouter.get(
   '/users/:_id',
   celebrate({
     params: Joi.object().keys({
-      _Id: Joi.string().length(24).hex().required(),
+      _id: Joi.string().length(24).hex().required(),
     }),
   }),
   getUser,
